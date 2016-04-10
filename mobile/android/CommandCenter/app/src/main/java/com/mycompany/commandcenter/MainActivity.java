c568d0a5a61a34c5b0ea201f73671805c5c01d26
Arash Nabili <arash.nabili@gmail.com>
2016-04-09 21:02:28 -0700

    Added command center app for debug purposes

---
+++ mobile/android/CommandCenter/app/src/main/java/com/mycompany/commandcenter/MainActivity.java
@@ -0,0 +1,445 @@
+package com.mycompany.commandcenter;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
+import com.o3dr.android.client.ControlTower;
+import com.o3dr.android.client.Drone;
+import com.o3dr.android.client.apis.ControlApi;
+import com.o3dr.android.client.apis.VehicleApi;
+import com.o3dr.android.client.interfaces.DroneListener;
+import com.o3dr.android.client.interfaces.TowerListener;
+import com.o3dr.services.android.lib.coordinate.LatLong;
+import com.o3dr.services.android.lib.coordinate.LatLongAlt;
+import com.o3dr.services.android.lib.drone.attribute.AttributeEvent;
+import com.o3dr.services.android.lib.drone.attribute.AttributeType;
+import com.o3dr.services.android.lib.drone.connection.ConnectionParameter;
+import com.o3dr.services.android.lib.drone.connection.ConnectionResult;
+import com.o3dr.services.android.lib.drone.connection.ConnectionType;
+import com.o3dr.services.android.lib.drone.property.Altitude;
+import com.o3dr.services.android.lib.drone.property.Gps;
+import com.o3dr.services.android.lib.drone.property.Home;
+import com.o3dr.services.android.lib.drone.property.Speed;
+import com.o3dr.services.android.lib.drone.property.State;
+import com.o3dr.services.android.lib.drone.property.Type;
+import com.o3dr.services.android.lib.drone.property.VehicleMode;
+import com.o3dr.services.android.lib.model.SimpleCommandListener;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements DroneListener, TowerListener, ConnectionCallbacks, OnConnectionFailedListener {
+
+    private GoogleApiClient mGoogleApiClient;
+
+    private ControlTower controlTower;
+
+    private Drone drone;
+    private int droneType = Type.TYPE_UNKNOWN;
+    private final Handler handler = new Handler();
+    Spinner modeSelector;
+
+    private VehicleApi vehicleApi;
+    private ControlApi controlApi;
+    private SimpleCommandListener commandListener;
+
+    // 3DR Services Listener
+    @Override
+    public void onTowerConnected() {
+        this.controlTower.registerDrone(this.drone, this.handler);
+        this.drone.registerDroneListener(this);
+    }
+
+    @Override
+    public void onTowerDisconnected() {
+
+    }
+
+    @Override
+    public void onDroneEvent(String event, Bundle extras) {
+        switch (event) {
+            case AttributeEvent.STATE_CONNECTED:
+                alertUser("Drone Connected");
+                updateConnectedButton(this.drone.isConnected());
+                updateArmButton();
+                break;
+
+            case AttributeEvent.STATE_DISCONNECTED:
+                alertUser("Drone Disconnected");
+                updateConnectedButton(this.drone.isConnected());
+                updateArmButton();
+                break;
+
+            case AttributeEvent.STATE_UPDATED:
+            case AttributeEvent.STATE_ARMING:
+                updateArmButton();
+                updateConnectedButton(this.drone.isConnected());
+                updateTakeOffButton();
+                break;
+
+            case AttributeEvent.STATE_VEHICLE_MODE:
+                updateVehicleMode();
+                break;
+
+            case AttributeEvent.TYPE_UPDATED:
+                Type newDroneType = this.drone.getAttribute(AttributeType.TYPE);
+                if (newDroneType.getDroneType() != this.droneType) {
+                    this.droneType = newDroneType.getDroneType();
+                    updateVehicleModesForType(this.droneType);
+                }
+                break;
+
+            case AttributeEvent.SPEED_UPDATED:
+                updateAltitude();
+                updateSpeed();
+                break;
+
+            case AttributeEvent.HOME_UPDATED:
+                updateDistanceFromHome();
+                break;
+
+            default:
+                //Log.i("DRONE_EVENT", event);
+                break;
+        }
+    }
+
+    @Override
+    public void onDroneConnectionFailed(ConnectionResult result) {
+
+    }
+
+    @Override
+    public void onDroneServiceInterrupted(String errorMsg) {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Initialize the service manager
+        this.controlTower = new ControlTower(getApplicationContext());
+
+        this.drone = new Drone(getApplicationContext());
+
+        this.modeSelector = (Spinner) findViewById(R.id.modeSelect);
+        this.modeSelector.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                onFlightModeSelected(view);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+
+        this.commandListener = new SimpleCommandListener();
+
+        // Create an instance of GoogleAPIClient
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(com.google.android.gms.common.ConnectionResult result) {
+
+    }
+
+    @Override
+    public void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+        this.controlTower.connect(this);
+    }
+
+    @Override
+    public void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+        if (this.drone.isConnected()) {
+            this.drone.disconnect();
+            updateConnectedButton(false);
+        }
+        this.controlTower.unregisterDrone(this.drone);
+        this.controlTower.disconnect();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void alertUser(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    protected void updateConnectedButton(Boolean isConnected) {
+        State vehicleState = this.drone.getAttribute(AttributeType.STATE);
+        Button connectButton = (Button) findViewById(R.id.btnConnect);
+        if (isConnected) {
+            connectButton.setText("Disconnect");
+        } else {
+            connectButton.setText("Connect");
+        }
+
+        // Disconnect not allowed while drone is armed
+        if (vehicleState.isConnected() && vehicleState.isArmed()) {
+            connectButton.setEnabled(false);
+        } else {
+            connectButton.setEnabled(true);
+        }
+    }
+
+    protected void updateArmButton() {
+        State vehicleState = this.drone.getAttribute(AttributeType.STATE);
+        Button armButton = (Button) findViewById(R.id.btnArmTakeOff);
+
+        if (this.drone.isConnected()) {
+            armButton.setEnabled(true);
+        } else {
+            armButton.setEnabled(false);
+        }
+
+        if (vehicleState.isArmed()) {
+            // Connected and Armed
+            armButton.setText("DISARM");
+        } else if (vehicleState.isConnected()) {
+            // Connected but not Armed
+            armButton.setText("ARM");
+        }
+    }
+
+    protected void updateTakeOffButton() {
+        State vehicleState = this.drone.getAttribute(AttributeType.STATE);
+        Button takeOffButton = (Button) findViewById(R.id.btnTakeOff);
+
+        if (vehicleState.isArmed()) {
+            takeOffButton.setEnabled(true);
+            if (vehicleState.isFlying()) {
+                // Land
+                takeOffButton.setText("LAND");
+            } else {
+                // Take off
+                takeOffButton.setText("TAKE OFF");
+            }
+        } else {
+            takeOffButton.setEnabled(false);
+        }
+    }
+
+    public void onBtnConnectTap(View view) {
+        if (this.drone.isConnected()) {
+            this.drone.disconnect();
+            vehicleApi = null;
+            controlApi = null;
+        } else {
+            Bundle extraParams = new Bundle();
+            extraParams.putInt(ConnectionType.EXTRA_TCP_SERVER_PORT, 14550); // Set default port to 14550
+
+            ConnectionParameter connectionParams = new ConnectionParameter(ConnectionType.TYPE_UDP, extraParams, null);
+            this.drone.connect(connectionParams);
+            vehicleApi = VehicleApi.getApi(this.drone);
+            controlApi = ControlApi.getApi(this.drone);
+        }
+    }
+
+    public void onArmButtonTap(View view) {
+        State vehicleState = this.drone.getAttribute(AttributeType.STATE);
+
+        if (vehicleState.isConnected() && !vehicleState.isArmed()) {
+            // Connected but not Armed
+            vehicleApi.arm(true);
+            alertUser("Vehicle armed");
+        } else {
+            vehicleApi.arm(false);
+            alertUser("Vehicle disarmed");
+        }
+    }
+
+    public void onTakeOffButtonTap(View view) {
+        State vehicleState = this.drone.getAttribute(AttributeType.STATE);
+
+        if (vehicleState.isFlying()) {
+            // Land
+            vehicleApi.setVehicleMode(VehicleMode.COPTER_LAND);
+            alertUser("Landing");
+        } else if (vehicleState.isArmed()) {
+            // Take off
+            controlApi.takeoff(10, commandListener); // Default take off altitude is 10m
+            alertUser("Taking off");
+        }
+    }
+
+    public void onFlyToMeButtonTap(View view) {
+        State vehicleState = this.drone.getAttribute(AttributeType.STATE);
+
+        if (vehicleState.isFlying()) {
+            // Get device location
+            LatLong deviceLocation = getDeviceLocation();
+            // Go to device location
+            controlApi.goTo(deviceLocation, false, commandListener);
+        }
+    }
+
+    public void onReturnToLaunchButtonTap(View view) {
+        State vehicleState = this.drone.getAttribute(AttributeType.STATE);
+
+        if (vehicleState.isFlying()) {
+            // Return to Launch
+            vehicleApi.setVehicleMode(VehicleMode.COPTER_RTL);
+        }
+    }
+
+    public void onFlightModeSelected(View view) {
+        VehicleMode vehicleMode = (VehicleMode) this.modeSelector.getSelectedItem();
+        vehicleApi.setVehicleMode(vehicleMode);
+    }
+
+    protected void updateVehicleModesForType(int droneType) {
+        List<VehicleMode> vehicleModes = VehicleMode.getVehicleModePerDroneType(droneType);
+        ArrayAdapter<VehicleMode> vehicleModeArrayAdapter = new ArrayAdapter<VehicleMode>(this, android.R.layout.simple_spinner_item, vehicleModes);
+        vehicleModeArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.modeSelector.setAdapter(vehicleModeArrayAdapter);
+    }
+
+    protected void updateVehicleMode() {
+        State vehicleState = this.drone.getAttribute(AttributeType.STATE);
+        VehicleMode vehicleMode = vehicleState.getVehicleMode();
+        ArrayAdapter arrayAdapter = (ArrayAdapter) this.modeSelector.getAdapter();
+        this.modeSelector.setSelection(arrayAdapter.getPosition(vehicleMode));
+
+        Button flyToMeButton = (Button) findViewById(R.id.btnFlyToMe);
+        Button returnToLaunchButton = (Button) findViewById(R.id.btnRTL);
+
+        if (vehicleMode == VehicleMode.COPTER_GUIDED) {
+            flyToMeButton.setEnabled(true);
+            returnToLaunchButton.setEnabled(true);
+        } else {
+            flyToMeButton.setEnabled(false);
+            returnToLaunchButton.setEnabled(false);
+        }
+    }
+
+    protected void updateAltitude() {
+        TextView altitudeTextView = (TextView) findViewById(R.id.altitudeValueTextView);
+        Altitude droneAltitude = this.drone.getAttribute(AttributeType.ALTITUDE);
+        altitudeTextView.setText(String.format("%3.1f", droneAltitude.getAltitude()) + "m");
+    }
+
+    protected void updateSpeed() {
+        TextView speedTextView = (TextView) findViewById(R.id.speedValueTextView);
+        Speed droneSpeed = this.drone.getAttribute(AttributeType.SPEED);
+        speedTextView.setText(String.format("%3.1f", droneSpeed.getGroundSpeed()) + "m/s");
+    }
+
+    protected void updateDistanceFromHome() {
+        TextView distanceTextView = (TextView) findViewById(R.id.distanceValueTextView);
+        Altitude droneAltitude = this.drone.getAttribute(AttributeType.ALTITUDE);
+        double vehicleAltitude = droneAltitude.getAltitude();
+        Gps droneGps = this.drone.getAttribute(AttributeType.GPS);
+        LatLong vehiclePosition = droneGps.getPosition();
+
+        double distanceFromHome = 0;
+
+        if (droneGps.isValid()) {
+            LatLongAlt vehicle3DPosition = new LatLongAlt(vehiclePosition.getLatitude(), vehiclePosition.getLongitude(), vehicleAltitude);
+            Home droneHome = this.drone.getAttribute(AttributeType.HOME);
+            distanceFromHome = distanceBetweenPoints(droneHome.getCoordinate(), vehicle3DPosition);
+        } else {
+            distanceFromHome = 0;
+        }
+
+        distanceTextView.setText(String.format("%3.1f", distanceFromHome) + "m");
+    }
+
+    protected double distanceBetweenPoints(LatLongAlt pointA, LatLongAlt pointB) {
+        if (pointA == null || pointB == null) {
+            return 0;
+        }
+        double dx = pointA.getLatitude() - pointB.getLatitude();
+        double dy = pointA.getLongitude() - pointB.getLongitude();
+        double dz = pointA.getAltitude() - pointB.getAltitude();
+        return Math.sqrt(dx*dx + dy*dy + dz*dz);
+    }
+
+    public LatLong getDeviceLocation() {
+        double latitude = 0;
+        double longitude = 0;
+        // Check for permission to access device's fine location
+        int permissionCheck = ContextCompat.checkSelfPermission(this, "android.permission.ACCESS_FINE_LOCATION");
+        if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            // App has permission to access device's fine location
+            // Get device's fine location and store it in location
+            // location stores coordinates in the form "latitude, longitude"
+            System.out.println("Permission granted");
+            Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (mLastLocation != null) {
+                latitude = mLastLocation.getLatitude();
+                longitude = mLastLocation.getLongitude();
+            }
+        }
+        else {
+            System.out.println("Permission denied");
+        }
+
+        LatLong location = new LatLong(latitude, longitude);
+        return location;
+    }
+}
